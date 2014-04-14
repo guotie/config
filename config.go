0a08581a98dd9d7167e0cccd3608d35492d46eae
guotie <guotie.9@gmail.com>
2014-04-14 11:06:49 +0800

    增加对任意类型的Scan操作

--- config.go
+++ config.go
@@ -2,13 +2,20 @@ package config
 
 import (
 	"encoding/json"
+	"errors"
+	"fmt"
 	"io/ioutil"
+	"reflect"
+	"strings"
 )
 
 var (
-	cfg = make(map[string]interface{})
+	cfg         = make(map[string]interface{})
+	KeyNotExist = errors.New("key not exist")
 )
 
+// read config file
+//
 func ReadCfg(fn string) {
 	if fn == "" {
 		fn = "./config.json"
@@ -23,6 +30,8 @@ func ReadCfg(fn string) {
 	}
 }
 
+// Get
+// if the key do not exist, return nil
 func Get(key string) interface{} {
 	if v, ok := cfg[key]; ok {
 		return v
@@ -31,6 +40,19 @@ func Get(key string) interface{} {
 	return nil
 }
 
+// GetMust
+// if the key do not exist, panic
+func GetMust(key string) interface{} {
+	v, ok := cfg[key]
+	if !ok {
+		panic("Not found " + key + " in config.")
+	}
+
+	return v
+}
+
+// GetString
+// Get string type by key
 func GetString(key string) string {
 	v := Get(key)
 	if v == nil {
@@ -43,6 +65,8 @@ func GetString(key string) string {
 	return ""
 }
 
+// GetString
+// Get string type by key, if not exist, panic
 func GetStringMust(key string) string {
 	v := Get(key)
 	if v == nil {
@@ -57,6 +81,8 @@ func GetStringMust(key string) string {
 	return ""
 }
 
+// GetString
+// Get string type by key, if key not exist, return the default
 func GetStringDefault(key, def string) string {
 	s := GetString(key)
 	if s == "" {
@@ -65,6 +91,7 @@ func GetStringDefault(key, def string) string {
 	return s
 }
 
+// GetInt
 func GetInt(key string) (int64, bool) {
 	v := Get(key)
 	if v == nil {
@@ -111,4 +138,382 @@ func GetFloat(key string) (float64, bool) {
 	}
 
 	return 0.0, false
+}
+
+// Scan
+//
+func Scan(key string, dest interface{}) (err error) {
+	val, ok := cfg[key]
+	if !ok {
+		return KeyNotExist
+	}
+
+	rv := reflect.ValueOf(dest)
+	if rv.Kind() != reflect.Ptr || rv.IsNil() {
+		return fmt.Errorf("param must be pointer and not nil")
+	}
+
+	switch reflect.Indirect(rv).Kind() {
+	case reflect.Map:
+		err = decodeMap(val, rv)
+
+	case reflect.Struct:
+		err = decodeStruct(val, rv)
+
+	case reflect.Slice:
+		err = decodeSlice(val, rv)
+
+	case reflect.Array:
+		err = decodeArray(val, rv)
+
+	default:
+		return fmt.Errorf("Unsupport type: %s", rv.Kind().String())
+	}
+
+	return
+}
+
+func decodeMap(val interface{}, rv reflect.Value) (err error) {
+	mval := val.(map[string]interface{})
+	//fmt.Println("decode map:", mval)
+
+	rv = reflect.Indirect(rv)
+	if rv.IsNil() {
+		//rv = reflect.MakeMap(rv.Type())
+		rv.Set(reflect.MakeMap(rv.Type()))
+	}
+
+	keytyp := rv.Type().Key().Kind()
+	if keytyp != reflect.String {
+		return fmt.Errorf("map key's type must be string: %s",
+			keytyp.String())
+	}
+
+	eletyp := rv.Type().Elem().Kind()
+	//fmt.Println(rv.Type().String(), eletyp.String(), rv.Type().Elem())
+
+	for key, valfd := range mval {
+		switch eletyp {
+		case reflect.Map:
+			tmp := reflect.MakeMap(rv.Type().Elem())
+			err = decodeMap(valfd, tmp)
+			//fmt.Println("map entry:", tmp.Interface())
+			rv.SetMapIndex(reflect.ValueOf(key),
+				reflect.ValueOf(tmp.Interface()))
+
+		case reflect.Struct:
+			tmp := reflect.New(rv.Type().Elem())
+			err = decodeStruct(valfd, tmp)
+			rv.SetMapIndex(reflect.ValueOf(key),
+				reflect.ValueOf(reflect.Indirect(tmp).Interface()))
+
+		case reflect.Slice:
+			tmp := reflect.MakeSlice(rv.Type().Elem(), 0, 0)
+			err = decodeSlice(valfd, tmp)
+			rv.SetMapIndex(reflect.ValueOf(key),
+				reflect.ValueOf(reflect.Indirect(tmp).Interface()))
+
+		case reflect.Array:
+			tmp := reflect.New(reflect.TypeOf(eletyp))
+			err = decodeArray(valfd, tmp)
+			rv.SetMapIndex(reflect.ValueOf(key),
+				reflect.ValueOf(reflect.Indirect(tmp).Interface()))
+
+		case reflect.Int:
+			tmp := int(valfd.(float64))
+			rv.SetMapIndex(reflect.ValueOf(key), reflect.ValueOf(tmp))
+
+		case reflect.Int16:
+			tmp := int16(valfd.(float64))
+			rv.SetMapIndex(reflect.ValueOf(key), reflect.ValueOf(tmp))
+
+		case reflect.Int32:
+			tmp := int32(valfd.(float64))
+			rv.SetMapIndex(reflect.ValueOf(key), reflect.ValueOf(tmp))
+
+		case reflect.Int64:
+			tmp := int64(valfd.(float64))
+			rv.SetMapIndex(reflect.ValueOf(key), reflect.ValueOf(tmp))
+
+		case reflect.Int8:
+			tmp := int8(valfd.(float64))
+			rv.SetMapIndex(reflect.ValueOf(key), reflect.ValueOf(tmp))
+
+		case reflect.Float32:
+			tmp := float32(valfd.(float64))
+			rv.SetMapIndex(reflect.ValueOf(key), reflect.ValueOf(tmp))
+
+		case reflect.Float64:
+			tmp := valfd.(float64)
+			rv.SetMapIndex(reflect.ValueOf(key), reflect.ValueOf(tmp))
+
+		case reflect.Uint:
+			tmp := uint64(valfd.(float64))
+			rv.SetMapIndex(reflect.ValueOf(key), reflect.ValueOf(tmp))
+
+		case reflect.Uint16:
+			tmp := uint64(valfd.(float64))
+			rv.SetMapIndex(reflect.ValueOf(key), reflect.ValueOf(tmp))
+
+		case reflect.Uint32:
+			tmp := uint64(valfd.(float64))
+			rv.SetMapIndex(reflect.ValueOf(key), reflect.ValueOf(tmp))
+
+		case reflect.Uint64:
+			tmp := uint64(valfd.(float64))
+			rv.SetMapIndex(reflect.ValueOf(key), reflect.ValueOf(tmp))
+
+		case reflect.String:
+			tmp := valfd.(string)
+			rv.SetMapIndex(reflect.ValueOf(key), reflect.ValueOf(tmp))
+
+		case reflect.Bool:
+			tmp := valfd.(bool)
+			rv.SetMapIndex(reflect.ValueOf(key), reflect.ValueOf(tmp))
+		}
+		if err != nil {
+			return err
+		}
+	}
+
+	//fmt.Println("decode result:", rv.Interface())
+	return nil
+}
+
+func decodeStruct(val interface{}, rv reflect.Value) (err error) {
+	mval := val.(map[string]interface{})
+
+	if rv.Kind() == reflect.Ptr && rv.IsNil() {
+		rv.Set(reflect.New(rv.Type()))
+	}
+
+	rv = reflect.Indirect(rv)
+
+	typ := rv.Type()
+
+	var valfd interface{}
+	for i := 0; i < rv.NumField(); i++ {
+		fd := rv.Field(i)
+		tag := typ.Field(i).Tag.Get("json")
+		if tag == "-" {
+			continue
+		}
+		if tag != "" {
+			valfd = mval[tag]
+		} else {
+			valfd = mval[typ.Field(i).Name]
+			if valfd == nil {
+				valfd = mval[strings.ToLower(typ.Field(i).Name)]
+				if valfd == nil {
+					continue
+				}
+			}
+		}
+
+		switch fd.Kind() {
+		case reflect.Map:
+			err = decodeMap(valfd, fd)
+		case reflect.Struct:
+			err = decodeStruct(valfd, fd)
+		case reflect.Slice:
+			err = decodeSlice(valfd, fd)
+		case reflect.Array:
+			err = decodeArray(valfd, fd)
+
+		case reflect.Int:
+			fallthrough
+		case reflect.Int16:
+			fallthrough
+		case reflect.Int32:
+			fallthrough
+		case reflect.Int64:
+			fallthrough
+		case reflect.Int8:
+			fallthrough
+		case reflect.String:
+			fallthrough
+		case reflect.Float32:
+			fallthrough
+		case reflect.Float64:
+			fallthrough
+		case reflect.Uint:
+			fallthrough
+		case reflect.Uint16:
+			fallthrough
+		case reflect.Uint32:
+			fallthrough
+		case reflect.Uint64:
+			fallthrough
+		case reflect.Bool:
+			err = decodePrimary(valfd, fd)
+		default:
+		}
+
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func decodeSlice(val interface{}, rv reflect.Value) error {
+	sval := val.([]interface{})
+
+	rv = reflect.Indirect(rv)
+	if rv.IsNil() {
+		//fmt.Println("decodeSlice: nil slice, create new slice.", rv.Type().String())
+		rv.Set(reflect.New(rv.Type()).Elem())
+		//rv = reflect.MakeSlice(rv.Type(), 0, 0)
+	}
+
+	eletyp := rv.Type().Elem().Kind()
+	//fmt.Println("decode slice:", rv.Type().String(), eletyp.String(), rv.Type().Elem())
+
+	var (
+		err error
+		tmp reflect.Value
+	)
+	for i, valfd := range sval {
+		if i >= rv.Cap() {
+			newcap := rv.Cap() + rv.Cap()/2
+			if newcap < 4 {
+				newcap = 4
+			}
+			newv := reflect.MakeSlice(rv.Type(), rv.Len(), newcap)
+			reflect.Copy(newv, rv)
+			rv.Set(newv)
+		}
+		rv.SetLen(i + 1)
+
+		switch eletyp {
+		case reflect.Map:
+			//reflect.Make
+			tmp = reflect.MakeMap(rv.Type().Elem())
+			err = decodeMap(valfd, tmp)
+			rv.Index(i).Set(reflect.ValueOf(tmp.Interface()))
+
+		case reflect.Struct:
+			tmp = reflect.New(rv.Type().Elem())
+			err = decodeStruct(valfd, tmp)
+			rv.Index(i).Set(reflect.ValueOf(tmp.Elem().Interface()))
+
+		case reflect.Slice:
+			tmp = reflect.New(rv.Type().Elem())
+			err = decodeSlice(valfd, tmp)
+			rv.Index(i).Set(reflect.ValueOf(tmp.Elem().Interface()))
+
+		case reflect.Array:
+			tmp = reflect.New(reflect.TypeOf(eletyp))
+			err = decodeArray(valfd, tmp)
+			rv.Index(i).Set(reflect.ValueOf(tmp.Interface()))
+
+		case reflect.Int:
+			pv := int(valfd.(float64))
+			rv.Index(i).Set(reflect.ValueOf(pv))
+
+		case reflect.Int16:
+			pv := int16(valfd.(float64))
+			rv.Index(i).Set(reflect.ValueOf(pv))
+
+		case reflect.Int32:
+			pv := int32(valfd.(float64))
+			rv.Index(i).Set(reflect.ValueOf(pv))
+
+		case reflect.Int64:
+			pv := int64(valfd.(float64))
+			rv.Index(i).Set(reflect.ValueOf(pv))
+
+		case reflect.Int8:
+			pv := int8(valfd.(float64))
+			rv.Index(i).Set(reflect.ValueOf(pv))
+
+		case reflect.Float32:
+			pv := float32(valfd.(float64))
+			rv.Index(i).Set(reflect.ValueOf(pv))
+
+		case reflect.Float64:
+			pv := valfd.(float64)
+			rv.Index(i).Set(reflect.ValueOf(pv))
+
+		case reflect.Uint:
+			pv := uint64(valfd.(float64))
+			rv.Index(i).Set(reflect.ValueOf(pv))
+
+		case reflect.Uint16:
+			pv := uint64(valfd.(float64))
+			rv.Index(i).Set(reflect.ValueOf(pv))
+
+		case reflect.Uint32:
+			pv := uint64(valfd.(float64))
+			rv.Index(i).Set(reflect.ValueOf(pv))
+
+		case reflect.Uint64:
+			pv := uint64(valfd.(float64))
+			rv.Index(i).Set(reflect.ValueOf(pv))
+
+		case reflect.String:
+			pv := valfd.(string)
+			rv.Index(i).Set(reflect.ValueOf(pv))
+
+		case reflect.Bool:
+			pv := valfd.(bool)
+			rv.Index(i).Set(reflect.ValueOf(pv))
+		}
+
+		if err != nil {
+			return err
+		}
+		//rv = reflect.Append(rv,
+		//	reflect.ValueOf(tmp.Interface()))
+	}
+
+	//fmt.Println("decode slice result:", rv.Interface())
+	return nil
+}
+
+func decodeArray(val interface{}, rv reflect.Value) error {
+	panic("decode Array Not implement!")
+	return nil
+}
+
+func decodePrimary(val interface{}, rv reflect.Value) error {
+	irv := reflect.Indirect(rv)
+	switch irv.Kind() {
+	case reflect.Int:
+		fallthrough
+	case reflect.Int16:
+		fallthrough
+	case reflect.Int32:
+		fallthrough
+	case reflect.Int64:
+		fallthrough
+	case reflect.Int8:
+		rv.SetInt(int64(val.(float64)))
+
+	case reflect.Uint:
+		fallthrough
+	case reflect.Uint16:
+		fallthrough
+	case reflect.Uint32:
+		fallthrough
+	case reflect.Uint64:
+		rv.SetUint(uint64(val.(float64)))
+
+	case reflect.Float32:
+		fallthrough
+	case reflect.Float64:
+		rv.SetFloat(val.(float64))
+
+	case reflect.String:
+		rv.SetString(val.(string))
+
+	case reflect.Bool:
+		rv.SetBool(val.(bool))
+
+	default:
+		panic("No type: " + irv.Kind().String())
+	}
+
+	return nil
 }
--- config_test.go
+++ config_test.go
@@ -1,6 +1,8 @@
 package config
 
 import (
+	"fmt"
+	//"reflect"
 	"testing"
 )
 
@@ -26,4 +28,112 @@ func TestFloat(t *testing.T) {
 	if (!ok) || (ft != 1.67) {
 		t.Fatal("get float failed:", ok, ft)
 	}
+}
+
+func TestScan(t *testing.T) {
+	type Is struct {
+		Strin string `json:"str"`
+		Iin   int64
+	}
+	var s struct {
+		Inner Is
+		Str   string
+		I     int
+		Slc   []int
+	}
+	var (
+		m1 = make(map[string]int)
+		m2 = make(map[string]string)
+	)
+
+	err := Scan("ts", &s)
+	if err != nil {
+		t.Fatal(err.Error())
+	}
+	fmt.Println(s)
+	//println(s.Inner.Strin, s.Inner.Iin, s.Str, s.I)
+	err = Scan("tm1", &m1)
+	fmt.Println(m1)
+
+	err = Scan("tm2", &m2)
+	fmt.Println(m2)
+
+}
+
+func testScan2(t *testing.T) {
+	m3 := make(map[string]map[string]int)
+
+	err := Scan("tm3", &m3)
+	if err != nil {
+		t.Fatal(err.Error())
+	}
+	fmt.Println(m3)
+}
+
+func testScan3(t *testing.T) {
+	type IE3 struct {
+		Ie1 int
+		Ie2 string
+		Ie3 float32
+	}
+	m4 := make(map[string]IE3)
+	err := Scan("tm4", &m4)
+	if err != nil {
+		t.Fatal(err.Error())
+	}
+	fmt.Println("m4:", m4)
+
+	var m5 map[string]IE3
+	err = Scan("tm4", &m5)
+	fmt.Println("m5:", m5)
+}
+
+func testScan4(t *testing.T) {
+	var s1, is1 []int
+	var s2, is2 []string
+
+	err := Scan("sl1", &s1)
+	if err != nil {
+		t.Fatal(err.Error())
+	}
+	fmt.Println("s1:", s1)
+
+	is1 = make([]int, 0)
+	err = Scan("sl1", &is1)
+	if err != nil {
+		t.Fatal(err.Error())
+	}
+	fmt.Println("is1:", is1)
+
+	err = Scan("sl2", &s2)
+	if err != nil {
+		t.Fatal(err.Error())
+	}
+	fmt.Println("s2:", s2)
+
+	is2 = make([]string, 0)
+	err = Scan("sl2", &is2)
+	if err != nil {
+		t.Fatal(err.Error())
+	}
+	fmt.Println("is2:", is2)
+}
+
+func TestScan5(t *testing.T) {
+	type St struct {
+		I   int
+		Str string
+	}
+	var (
+		st1 []St
+		st2 []map[string]int
+		st3 [][]int
+	)
+
+	Scan("ss1", &st1)
+	fmt.Println("st1", st1)
+	Scan("ss2", &st2)
+	fmt.Println("st2", st2)
+	Scan("ss3", &st3)
+	fmt.Println("st3", st3)
 }
